6497f03ca150b308c0f444a6040cae7b537fe8e0
vselected <dominik.sarleja@gmail.com>
2024-10-02 15:52:00 +0200

    Learnig Strings

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-
 if __name__ == '__main__':
-    print('PyCharm')
+
+    my_string = "Hello World"
+    print(my_string)
+    print(my_string[0])
+    print(my_string[0:3])
+    print(my_string[-2])
+
+    my_string = "abcdefghijk"
+    print(my_string[2:])
+    print(my_string[:3])
+    print(my_string[3:6])
+    print(my_string[1:3])
+
 
 
