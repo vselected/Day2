1ca2e96bc4f533cdb4f49eb9c5aa6bf1c660fb76
vselected <dominik.sarleja@gmail.com>
2024-10-02 16:11:18 +0200

    Basic String Properties and Methods

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 if __name__ == '__main__':
 
+#String slicing and indexing
+
     my_string = "Hello World"
     print(my_string)
     print(my_string[0])
@@ -11,6 +13,26 @@ if __name__ == '__main__':
     print(my_string[:3])
     print(my_string[3:6])
     print(my_string[1:3])
+    print(my_string[::2])
+    print(my_string[::3])
+    print(my_string[2:9:2])
+    print(my_string[::-1])
 
+#Immutability
 
+    name = "Sam"
+#We want to change it to "Pam"
+    slice_name = name[1:]
+    name_last_letters = name[0]
+    name_last_letters = "P"
+    print(name_last_letters + slice_name)
 
+    x = "Hello World"
+    x = x + " it's raining"
+    print(x)
+
+    x = "Hello World"
+    print(x.upper())
+    print(x.lower())
+    print(x.split())
+    print(x)
