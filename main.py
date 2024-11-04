c1e206f28bb297d48a62a2b5d72eb83e92471c61
vselected <dominik.sarleja@gmail.com>
2024-11-04 14:06:13 +0100

    Learning about lists

---
+++ main.py
@@ -0,0 +1,9 @@
+# Create variable that hold a list
+my_list = ["String",1,2,3]
+print(my_list)
+print(len(my_list))
+print(my_list[0:2])
+
+# Create variable that hold another list
+another_list = ["four", "five", 5]
+print(my_list+another_list)
