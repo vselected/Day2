975d0421c4c59fda96ca2524455a065b66ee42ed
vselected <dominik.sarleja@gmail.com>
2024-11-04 15:19:27 +0100

    Dictionaries Exercise

--- main.py
+++ main.py
@@ -41,3 +41,21 @@ list = [1, "String", 2.342]
 
 # Dictionaries
 # {"key1":"value1", "key2":"value2"}
+
+my_dictionary = {"key1":"value1", "key2":"value2"}
+print(my_dictionary["key1"])
+
+prices_lookup = {"apple":2.99, "milk":5, "cheese":2.33}
+print(prices_lookup["apple"])
+d = {"k1":123, "k2":[2,4,2], "k3":{"kk1":2121, "kk2":"dsa"}}
+print(d["k3"]["kk2"].upper())
+print(d.keys())
+print(d.values())
+print(d.items())
+
+# Exercise Dictionaries
+# Create a dictionary where all the keys are strings, and all the values are integers.
+# For example:
+# {'Monday':19, 'Tuesday':20}
+
+dictionary = {"Apple":3, "Milk":2, "Bread":1}
